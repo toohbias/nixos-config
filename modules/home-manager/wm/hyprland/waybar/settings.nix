22f978b54b99ba7bd9d35a190313525867a233db
toohbias <db2tbo@outlook.de>
2025-04-11 18:28:57 +0200

    maybe itll work

--- modules/home-manager/wm/hyprland/waybar/settings.nix
+++ modules/home-manager/wm/hyprland/waybar/settings.nix
@@ -1,32 +1,28 @@
-let 
-
-  config = {
-    layer = "top";
-    position = "top";
-
-    modules_left = [
-      "hyprland/window"
-    ];
-
-    modules_center = [
-      "wlr/taskbar"
-      "hyprland/workspaces"
-    ];
-
-    modules_right = [
-      "cpu"
-      "pulseaudio"
-      "clock"
-    ];
-
-    "wlr/taskbar" = {
-      format = "{icon}";
-      tooltop = false;
-    };
-  };
-
-in {
-
-    programs.waybar.settings = builtins.toJSON config;
-
+{
+  programs.waybar.settings = [
+    {
+      layer = "top";
+      position = "top";
+    
+      modules_left = [
+        "hyprland/window"
+      ];
+    
+      modules_center = [
+        "wlr/taskbar"
+        "hyprland/workspaces"
+      ];
+    
+      modules_right = [
+        "cpu"
+        "pulseaudio"
+        "clock"
+      ];
+    
+      "wlr/taskbar" = {
+        format = "{icon}";
+        tooltop = false;
+      };
+    }
+  ];
 }
