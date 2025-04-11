c3f7f1458bcb5e2e851bda22e68ead883681411e
toohbias <db2tbo@outlook.de>
2025-04-11 21:07:34 +0200

    autogenerated commit message

--- modules/home-manager/wm/hyprland/waybar/style.nix
+++ modules/home-manager/wm/hyprland/waybar/style.nix
@@ -13,7 +13,12 @@
       border-bottom: 0px solid #000000;
     }
 
-
+    #custom-launcher {
+      background: #000000;
+      margin: 5px 5px 5px 5px;
+      padding: 5px 5px 5px 5px;
+      border-radius: 16px;
+    }
   '';
 
 }
