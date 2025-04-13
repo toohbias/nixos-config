240c445ade2b065ac349add92c86400ad497591f
toohbias <db2tbo@outlook.de>
2025-04-13 17:14:20 +0200

    autogenerated commit message

--- modules/home-manager/wm/hyprland/env.nix
+++ modules/home-manager/wm/hyprland/env.nix
@@ -2,6 +2,12 @@
 
   wayland.windowManager.hyprland.settings = {
     env = [
+      "NIXOS_OZONE_WL,1"
+      "XDG_CURRENT_DESKTOP,Hyprland"
+      "XDG_SESSION_TYPE,wayland"
+      "XDG_SESSION_DESKTOP,Hyprland"
+      "QT_QPA_PLATFORM,wayland"
+
       "XCURSOR_SIZE,24"
     ];
   };
