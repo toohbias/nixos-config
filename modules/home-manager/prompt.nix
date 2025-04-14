f2adf6a88116753b4d9890c24da8ecbf850aa2f3
toohbias <db2tbo@outlook.de>
2025-04-14 14:37:07 +0200

    autogenerated commit message

--- modules/home-manager/prompt.nix
+++ modules/home-manager/prompt.nix
@@ -4,12 +4,51 @@
     enable = true;
     enableBashIntegration = true;
 
-#   settings = {
-#     add_newline = false;
-#     format = lib.concatStrings [
-#       ""
-#     ];
-#   };
+    settings = {
+      add_newline = false;
+      format = lib.concatStrings [
+        "[](#161A1D)"
+        "$os"
+        "[](bg:#660708 fg:#161A1D)"
+        "$username"
+        "[](bg:#A4161A fg:#660708)"
+        "$directory"
+        "[](bg:#BA181B fg:#A4161A)"
+        "$git_branch"
+        "$git_status"
+        "[](#BA181B)"
+      ];
+      os = {
+        disabled = false;
+        style = "bg:161A1D";
+        format = "[$symbol]($style)";
+      };
+      username = {
+        disabled = false;
+        show_always = true;
+        style_user = "bg:#660708";
+        style_root = "bg:#660708 bold";
+        format = "[$user]($style)";
+      };
+      directory = {
+        disabled = false;
+        truncation_length = 3;
+        truncation_symbol = ".../";
+        style = "bg:#A4161A";
+        format = "[$path]($style)";
+      };
+      git_branch = {
+        disbled = false;
+        symbol = "";
+        style = "bg:#BA181B";
+        format = "[$symbol $branch]($style)";
+      };
+      git_status = {
+        disabled = false;
+        style = "bg:#BA181B";
+        format = "[$all_status]($style)";
+      };
+    };
   };
 
 }
