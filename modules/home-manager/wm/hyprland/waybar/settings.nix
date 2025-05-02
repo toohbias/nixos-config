3abfbb133f2c9b608b5600d3a373c328d9f78579
toohbias <db2tbo@outlook.de>
2025-05-03 00:14:12 +0200

    autogenerated commit message

--- modules/home-manager/wm/hyprland/waybar/settings.nix
+++ modules/home-manager/wm/hyprland/waybar/settings.nix
@@ -2,43 +2,13 @@
   programs.waybar.settings.mainBar = {
     layer = "top";
     position = "top";
-    height = 25;
+    height = 30;
 
-    modules_left = [
-      #       "custom/launcher"
-      #       "wlr/taskbar"
-    ];
-
-    modules_center = [
-      #       "hyprland/workspaces"
-    ];
+    output = ["HDMI-A-1"];
 
     modules_right = [
-      #       "cpu"
-      #       "pulseaudio"
       "clock"
     ];
-
-    #     "custom/launcher" = {
-    #       format = "";
-    #       on-click = "wofi --show drun";
-    #       tooltip = false;
-    #     };
-
-    #     "wlr/taskbar" = {
-    #       format = "{icon}";
-    #       on-click = "activate";
-    #     };
-
-    #     "hyprland/workspaces" = {
-    #       format = "{windows}";
-    #       window-rewrite-default = "";
-    #       window-rewrite = {
-    #         "class<ungoogled-chromium>" = "";
-    #         "class<ungoogled-chromium> title<.*github.*>" = "󰊤";
-    #         "kitty" = "";
-    #       };
-    #     };
 
     clock = {
       format = "{:%R  %A %b %d}";
