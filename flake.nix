65d692a537758d61b101dc4477428a383b9d13fc
toohbias <db2tbo@outlook.de>
2025-05-02 18:04:11 +0200

    autogenerated commit message

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My system configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
 
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
