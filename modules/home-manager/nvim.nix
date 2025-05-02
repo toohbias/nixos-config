17002c778f0d6e6615fadce9b1abef4b7fb73835
toohbias <db2tbo@outlook.de>
2025-05-02 22:14:55 +0200

    autogenerated commit message

--- modules/home-manager/nvim.nix
+++ modules/home-manager/nvim.nix
@@ -15,19 +15,6 @@
         autocomplete.blink-cmp.enable = true;
         filetree.neo-tree.enable = true;
 
-        keymaps = [
-          {
-            key = "<leader>ef";
-            mode = "n";
-            action = "<cmd>Neotree toggle<CR>";
-          }
-          {
-            key = "<leader>e";
-            mode = "n";
-            action = "<cmd>Neotree focus<CR>";
-          }
-        ];
-
         languages = {
           enableLSP = true;
           enableTreesitter = true;
