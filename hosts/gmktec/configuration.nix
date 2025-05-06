eef52fbf9c99eda99686b27c91794ce97aebf7f4
toohbias <db2tbo@outlook.de>
2025-05-06 19:21:27 +0200

    autogenerated commit message

--- hosts/gmktec/configuration.nix
+++ hosts/gmktec/configuration.nix
@@ -1,28 +1,25 @@
 {
-  imports =
-    [ 
-      ./hardware-configuration.nix
-      ./../../modules/nixos
-    ];
+  imports = [
+    ./hardware-configuration.nix
+    ./../../modules/nixos
+  ];
 
   nix.settings = {
     experimental-features = ["nix-command" "flakes"];
-#    substituters = ["https://hyprland.cachix.org"];
-#    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
   };
 
   security.polkit.enable = true;
 
-  networking.hostName = "nixos"; 
+  networking.hostName = "nixos";
 
-# services.xserver = {
-#   enable = true;
-#   desktopmanager = {
-#     xfce.enable = true;
-#   };
-#   displaymanager.defaultsession = "xfce";
-# };
-  
+  # services.xserver = {
+  #   enable = true;
+  #   desktopmanager = {
+  #     xfce.enable = true;
+  #   };
+  #   displaymanager.defaultsession = "xfce";
+  # };
+
   # some programs need suid wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
@@ -34,7 +31,5 @@
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
 
-  system.stateVersion = "24.11"; 
-
+  system.stateVersion = "24.11";
 }
-
