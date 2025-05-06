1c723fc9f2d6f6fa445f0301d7d60ac7b4adcde7
toohbias <db2tbo@outlook.de>
2025-05-06 16:53:21 +0200

    autogenerated commit message

--- modules/home-manager/wm/hyprland/waybar/style.nix
+++ modules/home-manager/wm/hyprland/waybar/style.nix
@@ -9,84 +9,25 @@
         background: transparent;
     }
 
-    #clock,
-    #pulseaudio
-    #cpu,
-    #memory,
-    #disk,
-    #network,
-    #battery {
-        padding: 0 10px;
-        color: #ffffff;
+    .modules-left {
+        padding: 5px;
+        margin: 5px;
+        border-radius: 10px;
+        background: rgba(0, 0, 0, 0.75);
     }
 
-    #window,
-    #workspaces {
-        margin: 0 4px;
+    .modules-center {
+        padding: 5px;
+        margin: 5px;
+        border-radius: 10px;
+        background: rgba(0, 0, 0, 0.75);
     }
 
-    /* If workspaces is the leftmost module, omit left margin */
-    .modules-left > widget:first-child > #workspaces {
-        margin-left: 0;
-    }
-
-    /* If workspaces is the rightmost module, omit right margin */
-    .modules-right > widget:last-child > #workspaces {
-        margin-right: 0;
-    }
-
-    #clock {
-        background-color: #64727D;
-    }
-
-    #battery {
-        background-color: #ffffff;
-        color: #000000;
-    }
-
-    #battery.charging, #battery.plugged {
-        color: #ffffff;
-        background-color: #26A65B;
-    }
-
-    #cpu {
-        background-color: #2ecc71;
-        color: #000000;
-    }
-
-    #memory {
-        background-color: #9b59b6;
-    }
-
-    #disk {
-        background-color: #964B00;
-    }
-
-    #network {
-        background-color: #2980b9;
-    }
-
-    #network.disconnected {
-        background-color: #f53c3c;
-    }
-
-    #pulseaudio {
-        background-color: #f1c40f;
-        color: #000000;
-    }
-
-    #pulseaudio.muted {
-        background-color: #90b1b1;
-        color: #2a5c45;
-    }
-
-    #wireplumber {
-        background-color: #fff0f5;
-        color: #000000;
-    }
-
-    #wireplumber.muted {
-        background-color: #f53c3c;
+    .modules-right {
+        padding: 5px;
+        margin: 5px;
+        border-radius: 10px;
+        background: rgba(0, 0, 0, 0.75);
     }
   '';
 }
