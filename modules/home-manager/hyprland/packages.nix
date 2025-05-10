98ebafad6139b9c070f7d3d1851fa37fc5770995
toohbias <db2tbo@outlook.de>
2025-05-10 22:09:41 +0200

    autogenerated commit message

--- modules/home-manager/hyprland/packages.nix
+++ modules/home-manager/hyprland/packages.nix
@@ -7,8 +7,6 @@
     pkgs.wofi
     pkgs.waybar
     pkgs.hyprpaper
-    pkgs.wl-clipboard
-    pkgs.cliphist
     inputs.mcmojave-hyprcursor.packages.${pkgs.stdenv.hostPlatform.system}.default
   ];
 }
