43090e8fe50cd1c1a97a0e1a4ecf47d442015172
toohbias <db2tbo@outlook.de>
2025-05-10 22:40:30 +0200

    autogenerated commit message

--- modules/home-manager/nvim.nix
+++ modules/home-manager/nvim.nix
@@ -11,6 +11,8 @@
           tabstop = 4;
         };
 
+        useSystemClipboard = true;
+
         telescope.enable = true;
         autocomplete.blink-cmp.enable = true;
         filetree.neo-tree.enable = true;
