a92f62c8bcd5fd9c7c53f2e08fe7c4cd15e3c002
toohbias <db2tbo@outlook.de>
2025-05-28 23:21:38 +0200

    autogenerated commit message

--- modules/nixos/sessions.nix
+++ modules/nixos/sessions.nix
@@ -1,31 +1,11 @@
-{ pkgs, ... }: {
+{
 
   services.greetd = {
     enable = true;
     settings = {
-      initial_session = {
+      default_session = {
         command = "Hyprland";
         user = "tobi";
-      };
-      default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --sessions";
-        user = "greeter";
-      };
-    };
-  };
-
-  programs.uwsm = {
-    enable = true;
-    waylandCompositors = {
-      hyprland = {
-        prettyName = "Hyprland";
-        comment = "Hyprland compositor managed by UWSM";
-        binPath = "/run/current-system/sw/bin/Hyprland";
-      };
-      xfce = {
-        prettyName = "Xfce";
-        comment = "Xfce compositor managed by UWSM";
-        binPath = "/run/current-system/sw/bin/startxfce4";
       };
     };
   };
@@ -40,6 +20,7 @@
 
   programs.hyprland = {
     enable = true;
+    withUWSM = true;
     xwayland.enable = true;
   };
 
