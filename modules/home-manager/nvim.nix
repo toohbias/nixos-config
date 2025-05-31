7b75f0a95ac65c5a3b7f764d05ceebb6dc655766
toohbias <db2tbo@outlook.de>
2025-05-31 10:44:22 +0200

    autogenerated commit message

--- modules/home-manager/nvim.nix
+++ modules/home-manager/nvim.nix
@@ -38,6 +38,7 @@
             setupOpts = {
               backend = "kitty";
               integrations.markdown.onlyRenderAtCursor = true;
+            };
           };
           motion.flash-nvim.enable = true;
         };
