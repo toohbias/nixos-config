f05760bd0c77b6d4db4987c364ae13b4ffb6317f
toohbias <db2tbo@outlook.de>
2025-07-30 00:14:20 +0200

    autogenerated commit message

--- modules/home-manager/hyprland/windowrules.nix
+++ modules/home-manager/hyprland/windowrules.nix
@@ -2,12 +2,14 @@
   wayland.windowManager.hyprland.settings = {
     windowrule = [
       "opaque, title:(.*)(Chromium)$"
+      "opaque, title:Test"
     ];
 
     windowrulev2 = [
       "suppressevent maximize, class:.*"
       "nofocus, class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
       "float, class:(src)(.*)$"
+      "float, title:Test"
     ];
   };
 }
