1fbbc7c708edbc55b9c3cd91cf17fdbab7d4795e
toohbias <db2tbo@outlook.de>
2025-07-30 21:27:22 +0200

    autogenerated commit message

--- hosts/gmktec/configuration.nix
+++ hosts/gmktec/configuration.nix
@@ -12,14 +12,6 @@
 
   networking.hostName = "nixos";
 
-  # some programs need suid wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
 
