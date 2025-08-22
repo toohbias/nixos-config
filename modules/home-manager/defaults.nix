0fabe756866c5fa46db4a6d50a784ac54dec5e45
toohbias <db2tbo@outlook.de>
2025-08-22 23:54:02 +0200

    autogenerated commit message

--- modules/home-manager/defaults.nix
+++ modules/home-manager/defaults.nix
@@ -23,6 +23,8 @@
         "text/html" = "chromium-browser.desktop";
         "x-scheme-handler/http" = "chromium-browser.desktop";
         "x-scheme-handler/https" = "chromium-browser.desktop";
+        "x-scheme-handler/about" = "chromium-browser.desktop";
+        "x-scheme-handler/unknown" = "chromium-browser.desktop";
       };
     };
 
