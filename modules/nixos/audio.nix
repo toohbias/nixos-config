f57a3a1f439cace2499e5267944ff8c7d243229b
toohbias <db2tbo@outlook.de>
2026-04-01 11:50:14 +0200

    autogenerated commit message

--- modules/nixos/audio.nix
+++ modules/nixos/audio.nix
@@ -7,6 +7,23 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     jack.enable = true;
+
+    wireplumber.extraConfig = {
+      "10-default-source" = {
+        "monitor.alsa.rules" = [
+          {
+            matches = [
+              { "node.name" = "alsa_input.usb-BLUE_MICROPHONE_Blue_Snowball_797_2020_12_16_41638-00.mono-fallback" }
+            ];
+            actions = {
+              update-props = {
+                "priority.session" = 2500;
+              };
+            };
+          }
+        ];
+      };
+    };
   };
 
   programs.noisetorch.enable = true;
