8933d7f14849bbd0bbbfca54d5efab8e5b6d0a04
toohbias <db2tbo@outlook.de>
2026-04-01 12:22:12 +0200

    autogenerated commit message

--- modules/home-manager/hyprland/autostart.nix
+++ modules/home-manager/hyprland/autostart.nix
@@ -5,7 +5,6 @@
       "waybar"
       "wl-paste --type text --watch cliphist store"
       "wl-paste --type image --watch cliphist store"
-      "sleep 5 && noisetorch -i > /tmp/noisetorch.log 2>&1"
     ];
   };
 }
