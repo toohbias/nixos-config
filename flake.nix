6095d51bd1cbed4b5a9c5733e572820560b5eed5
toohbias <db2tbo@outlook.de>
2026-04-21 20:50:50 +0200

    autogenerated commit message

--- flake.nix
+++ flake.nix
@@ -4,14 +4,16 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
 
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
 
     nvf = {
       url = "github:notashelf/nvf";
-      # inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
 
     mcmojave-hyprcursor.url = "github:libadoxon/mcmojave-hyprcursor";
