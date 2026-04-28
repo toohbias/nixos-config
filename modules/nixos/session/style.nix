95d923f35904757690f5152b67e5619e90038b10
toohbias <db2tbo@outlook.de>
2026-04-29 00:12:10 +0200

    autogenerated commit message

--- modules/nixos/session/style.nix
+++ modules/nixos/session/style.nix
@@ -5,10 +5,30 @@
         font-family: FiraCode Nerd Font;
         font-weight: 500;
         font-size: 15px;
-
+        color: rgba(255, 255, 255, 1.0);
+    }
+    frame {
         background: rgba(0, 0, 0, 0.75);
         border-radius: 10px;
         border: 2px solid rgba(255, 255, 255, 1.0);
+    }
+    entry,
+    button {
+        background: rgba(0, 0, 0, 0);
+        border-radius: 10px;
+        border: 2px solid rgba(255, 255, 255, 1.0);
+    }
+    combobox {
+        border: none;
+    }
+    button.destructive-action {
+        border: 2px solid rgba(186, 24, 27, 1.0);
+    }
+    button.toggle {
+        border: 2px solid rgba(89, 89, 89, 1.0);
+    }
+    infobar {
+        visibility: hidden;
     }
   '';
 
