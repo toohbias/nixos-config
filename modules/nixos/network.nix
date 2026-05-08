53c068b3658288808713c42f12bce8e61a70fcc7
toohbias <db2tbo@outlook.de>
2026-05-08 22:13:33 +0200

    autogenerated commit message

--- modules/nixos/network.nix
+++ modules/nixos/network.nix
@@ -2,6 +2,8 @@
 
   networking.firewall = {
     enable = true;
+    allowedTCPPorts = [ 7000 7001 7100 ];
+    allowedUDPPorts = [ 5353 6000 6001 7011 ];
     trustedInterfaces = [ "virbr0" ];
   };
 
