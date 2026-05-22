a0be9712aaa6b8934b89e21f3d634cb0b529b254
toohbias <db2tbo@outlook.de>
2026-05-22 11:59:22 +0200

    autogenerated commit message

--- modules/nixos/xfce.nix
+++ modules/nixos/xfce.nix
@@ -4,9 +4,12 @@
     enable = true;
     desktopManager = {
       xfce.enable = true;
-      xterm.enable = true;
+      xterm.enable = false;
     };
   };
+    
+  services.libinput.enable = true;
+  services.displayManager.defaultSession = "xfce";
 
   environment.systemPackages = with pkgs; [
     xorg.xinit
