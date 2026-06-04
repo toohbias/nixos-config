c66e08139cd6d09fde12606cd63cb4dc63354d65
toohbias <db2tbo@outlook.de>
2026-06-04 22:44:37 +0200

    autogenerated commit message

--- modules/home-manager/tobi/hyprland/packages.nix
+++ modules/home-manager/tobi/hyprland/packages.nix
@@ -5,7 +5,6 @@
 }: {
   home.packages = [
     pkgs.wofi
-    pkgs.waybar
     pkgs.hyprpaper
     pkgs.hyprshot
     inputs.mcmojave-hyprcursor.packages.${pkgs.stdenv.hostPlatform.system}.default
--- modules/home-manager/tobi/hyprland/waybar/default.nix
+++ modules/home-manager/tobi/hyprland/waybar/default.nix
@@ -1,7 +1,8 @@
-{
+{ waybar-fixed, pkgs, ...}: {
 
   programs.waybar = {
     enable = true;
+    package = waybar-fixed.packages.${pkgs.stdenv.hostPlatform.system}.default;
   };
 
   imports = [
