6b2479385588d0a1a40df3c4ec8c6455559b85af
toohbias <db2tbo@outlook.de>
2026-06-13 17:23:55 +0200

    autogenerated commit message

--- modules/nixos/audio.nix
+++ modules/nixos/audio.nix
@@ -6,24 +6,24 @@
     pulse.enable = true;
     jack.enable = true;
 
-    wireplumber.extraConfig = {
-      "10-default-source" = {
-        "monitor.alsa.rules" = [
-          {
-            matches = [
-              {
-                "node.name" = "alsa_input.usb-BLUE_MICROPHONE_Blue_Snowball_797_2020_12_16_41638-00.mono-fallback";
-              }
-            ];
-            actions = {
-              update-props = {
-                "priority.session" = 2500;
-              };
-            };
-          }
-        ];
-      };
-    };
+    # wireplumber.extraConfig = {
+    #   "10-default-source" = {
+    #     "monitor.alsa.rules" = [
+    #       {
+    #         matches = [
+    #           {
+    #             "node.name" = "alsa_input.usb-BLUE_MICROPHONE_Blue_Snowball_797_2020_12_16_41638-00.mono-fallback";
+    #           }
+    #         ];
+    #         actions = {
+    #           update-props = {
+    #             "priority.session" = 2500;
+    #           };
+    #         };
+    #       }
+    #     ];
+    #   };
+    # };
   };
 
   programs.noisetorch.enable = true;
