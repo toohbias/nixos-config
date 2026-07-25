dd901c34311fc63f8361fe574bfa402ccba37100
toohbias <db2tbo@outlook.de>
2026-07-25 22:11:15 +0200

    autogenerated commit message

--- modules/nixos/steam.nix
+++ modules/nixos/steam.nix
@@ -1,9 +1,14 @@
-{
+{ pkgs, ... }: {
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
     localNetworkGameTransfers.openFirewall = true;
+  };
+
+  programs.nix-ld = {
+    enable = true;
+    libraries = pkgs.steam-run.fhsenv.args.multiPkgs pkgs;
   };
 
   nixpkgs.config.allowUnfree = true;
