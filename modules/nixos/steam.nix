f68a5d07e345c395ab3a19f0560ef69b9e9b84a4
toohbias <db2tbo@outlook.de>
2026-07-28 12:03:54 +0200

    autogenerated commit message

--- modules/nixos/steam.nix
+++ modules/nixos/steam.nix
@@ -6,9 +6,30 @@
     localNetworkGameTransfers.openFirewall = true;
   };
 
+  environment.systemPackages = with pkgs; [
+    steam-run
+  ];
+
   programs.nix-ld = {
     enable = true;
-    libraries = pkgs.steam-run.fhsenv.args.multiPkgs pkgs;
+    libraries = with pkgs; [
+      # https://github.com/NixOS/nixpkgs/blob/nixos-26.05/pkgs/by-name/st/steam/package.nix#L68
+      glibc
+      libxcrypt
+      libGL
+
+      libdrm
+      libgbm
+      udev
+      libudev0-shim
+      libva
+      vulkan-loader
+
+      networkmanager
+      # not documented, used for network status things in Big Picture
+      # FIXME: figure out how to only build libnm?
+      libcap # not documented, required by srt-bwrap
+    ];
   };
 
   nixpkgs.config.allowUnfree = true;
