9c42acb056c2cf4f522272864edcfbf4d1141a62
toohbias <db2tbo@outlook.de>
2026-08-20 21:59:39 +0200

    autogenerated commit message

--- modules/home-manager/tobi/hyprland/packages.nix
+++ modules/home-manager/tobi/hyprland/packages.nix
@@ -4,14 +4,12 @@
   ...
 }:
 {
-  home.packages =
-    with pkgs;
-    [
-      wofi
-      hyprpaper
-      hyprshot
-      tesseract
-      libnotify
-    ]
-    // inputs.mcmojave-hyprcursor.packages.${pkgs.stdenv.hostPlatform.system}.default;
+  home.packages = with pkgs; [
+    wofi
+    hyprpaper
+    hyprshot
+    tesseract
+    libnotify
+    inputs.mcmojave-hyprcursor.packages.${pkgs.stdenv.hostPlatform.system}.default
+  ];
 }
